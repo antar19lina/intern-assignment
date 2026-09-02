f85f8b900815007ad12c44f29b95c382d264a80f
Antarlina Balmiki <150431306+antar19lina@users.noreply.github.com>
2026-09-02 16:46:45 +0530

    Update SummaryCards.jsx

--- src/components/SummaryCards.jsx
+++ src/components/SummaryCards.jsx
@@ -3,10 +3,12 @@ import React from "react";
 export default function SummaryCards({ members, expenses }) {
   const totalExpense = expenses.reduce((sum, e) => sum + Number(e.amount), 0);
 
-  
-  const uniqueMembers = [...new Set(members.map(m => m.name))];
-  const avgPerPerson = uniqueMembers.length > 0 
-    ? (totalExpense / uniqueMembers.length).toFixed(2) 
+  const uniqueMembersInSplits = [
+    ...new Set(expenses.flatMap(e => e.splitWith))
+  ];
+
+  const avgPerPerson = uniqueMembersInSplits.length > 0
+    ? (totalExpense / uniqueMembersInSplits.length).toFixed(2)
     : 0;
 
   return (
